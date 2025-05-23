acd3d9d4b87ad0d91ec679e6d345f8ef411444e3
Nguyễn Bùi Mẫn Nhi <nhinbm.dev@gmail.com>
2025-05-24 00:23:28 +0700

    feat: album detail

--- app/pages/album-detail/album-detail.tsx
+++ app/pages/album-detail/album-detail.tsx
@@ -1,6 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
+import { useLocation } from "react-router";
 import CardDetail from "~/components/card-detail";
+import { getAlbumById } from "~/services/albums.service";
+import styles from "./album-data.module.css";
+import { getPhotosByAlbumId } from "~/services/photos.service";
+import { Image, List } from "antd";
 
 const AlbumDetailPage: React.FC = () => {
-  return <CardDetail></CardDetail>;
+  const location = useLocation();
+  const pathSnippets = location.pathname.split("/").filter((i) => i);
+  const id = parseInt(pathSnippets[1]);
+
+  const { data: albumData, isLoading: isAlbumLoading } = useQuery({
+    enabled: !!id,
+    queryKey: ["album", id],
+    queryFn: () => getAlbumById(id),
+  });
+
+  const albumId = albumData?.id;
+
+  const { data: photos, isLoading: isPhotosLoading } = useQuery({
+    enabled: !!albumId,
+    queryKey: ["photos", albumId],
+    queryFn: () => getPhotosByAlbumId(albumId!),
+  });
+
+  const getSafeThumbnailUrl = (url: string) => {
+    if (url.startsWith("https://via.placeholder.com")) {
+      return url.replace(
+        "https://via.placeholder.com",
+        "https://dummyjson.com/image"
+      );
+    }
+    return url;
+  };
+
+  if (isAlbumLoading || isPhotosLoading) return <div>Loading...</div>;
+  if (!albumData || !photos) return <div>Error loading data</div>;
+
+  return (
+    <CardDetail>
+      <h4 className={styles.albumData__header}>{albumData.title}</h4>
+      <List
+        grid={{ gutter: 16, column: 4 }}
+        dataSource={photos}
+        renderItem={(photo) => (
+          <List.Item key={photo.id}>
+            <Image
+              src={getSafeThumbnailUrl(photo.thumbnailUrl)}
+              alt={photo.title}
+              style={{ cursor: "pointer" }}
+            />
+          </List.Item>
+        )}
+      />
+    </CardDetail>
+  );
 };
 export default AlbumDetailPage;
