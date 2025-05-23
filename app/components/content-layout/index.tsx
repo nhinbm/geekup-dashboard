a69c81b9bda04fa0a5bf8045ad3f551ccf7e88c8
Nguyễn Bùi Mẫn Nhi <nhinbm.dev@gmail.com>
2025-05-23 22:00:14 +0700

    feat: update return back

--- app/components/content-layout/index.tsx
+++ app/components/content-layout/index.tsx
@@ -1,5 +1,8 @@
-import { Fragment, type ReactNode } from "react";
+import { type ReactNode } from "react";
 import styles from "./content-layout.module.css";
+import { LeftOutlined } from "@ant-design/icons";
+import { useLocation, useNavigate } from "react-router";
+import { Button } from "antd";
 
 interface ContentLayoutProps {
   header?: string;
@@ -7,9 +10,30 @@ interface ContentLayoutProps {
 }
 
 const ContentLayout: React.FC<ContentLayoutProps> = ({ header, children }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const pathSnippets = location.pathname.split("/").filter((i) => i);
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
     <>
-      {header && <h4 className={styles.contentLayout__header}>{header}</h4>}
+      {header && (
+        <h4 className={styles.contentLayout__header} onClick={handleBack}>
+          {pathSnippets.length >= 2 && (
+            <Button
+              icon={<LeftOutlined />}
+              onClick={handleBack}
+              type="text"
+              style={{ marginRight: "16px" }}
+            />
+          )}
+          {header}
+        </h4>
+      )}
       <div className={header && styles.contentLayout__content}>{children}</div>
     </>
   );
