64044cd04425b561b3955715fa3cec4e9588d94b
Nguyễn Bùi Mẫn Nhi <nhinbm.dev@gmail.com>
2025-05-23 18:49:24 +0700

    chore: update env

--- app/services/albums.service.ts
+++ app/services/albums.service.ts
@@ -1,16 +1,15 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL;
+
 export const getAlbums = async (page: number, pageSize: number) => {
   const start = (page - 1) * pageSize;
-  const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/albums",
-    {
-      params: {
-        _start: start,
-        _limit: pageSize,
-      },
-    }
-  );
+  const response = await axios.get(`${baseURL}/albums`, {
+    params: {
+      _start: start,
+      _limit: pageSize,
+    },
+  });
 
   return {
     data: response.data,
